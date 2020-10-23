fc8700fedfe2578d64841026b586b77658be4e34
seoyun <newseoyun@gmail.com>
2020-10-24 00:28:28 +0900

    숫자야구 만드는 중

---
+++ 3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+    <form @submit.prevent="onSubmitForm">
+      <input ref="answer" type="number" minlength="4" maxlength="4" v-model="value"/>
+      <button>입력</button>
+    </form>
+    <div>시도: {{tries.length}}</div>
+    <ul>
+      <li v-for="t in tries">
+        <div>{{t.try}}</div>
+        <div>{{t.result}}</div>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<
+<script>
+export default {
+  data() {
+    return {
+      tries: [],
+      value: '',
+      result: '',
+    }
+  },
+  methods: {
+    onSubmitForm(e) {
+      this.tries.push({
+        try: this.value,
+        result: '홈런'
+      });
+      this.value = '';
+      this.$refs.answer.focus();
+    }
+  }
+}
+</script>
